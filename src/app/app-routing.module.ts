1031bbd11a5e998b0a84971fb40db0f302a0b2a7
aman <amansaini1407@gmail.com>
2023-03-20 21:03:52 +0530

    added files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalenderComponent } from './compos/calender/calender.component';
+import { CardsComponent } from './compos/cards/cards.component';
+import { LaptopsComponent } from './compos/laptops/laptops.component';
+import { LoginComponent } from './compos/login/login.component';
+import { MobilesComponent } from './compos/mobiles/mobiles.component';
+import { SignupComponent } from './compos/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"login",component:LoginComponent
+  },
+  {
+    path:"signup",component:SignupComponent
+  },
+  {
+    path:"",component:CalenderComponent , pathMatch:'full'
+  },
+    {
+        path:"calender",component:CalenderComponent
+      },
+      {
+        path:"mobiles",component:MobilesComponent
+      },
+      {
+        path:"cards",component:CardsComponent
+      },
+      {
+        path:"laptops",component:LaptopsComponent
+      }
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { NavbarComponent } from './compos/navbar/navbar.component';
+import { CalenderComponent } from './compos/calender/calender.component';
+import { CardsComponent } from './compos/cards/cards.component';
+import { LaptopsComponent } from './compos/laptops/laptops.component';
+import { MobilesComponent } from './compos/mobiles/mobiles.component';
+import { LoginComponent } from './compos/login/login.component';
+import { SignupComponent } from './compos/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CalenderComponent,
+    CardsComponent,
+    LaptopsComponent,
+    MobilesComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
